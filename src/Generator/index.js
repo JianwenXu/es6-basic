de3146c3b797051a4f00c43b8fb54d287c5af833
xujianwen <xujianwen@ruijie.com.cn>
2019-11-01 15:23:52 +0800

    Generator 基本概念

--- src/Generator/index.js
+++ src/Generator/index.js
@@ -6,7 +6,19 @@ function* helloWorldGenerator() {
 
 var hw = helloWorldGenerator();
 
-console.log(hw.next());
-console.log(hw.next());
-console.log(hw.next());
-console.log(hw.next());
+console.log(hw.next()); // { value: 'hello', done: false }
+console.log(hw.next()); // { value: 'world', done: false }
+console.log(hw.next()); // { value: 'ending', done: true }
+console.log(hw.next()); // { value: undefined, done: true }
+
+function* helloWorldGenerator2() {
+  yield 'hello';
+  yield 'world';
+}
+
+var hw2 = helloWorldGenerator2();
+
+console.log(hw2.next()); // { value: 'hello', done: false }
+console.log(hw2.next()); // { value: 'world', done: false }
+console.log(hw2.next()); // { value: undefined, done: true }
+console.log(hw2.next()); // { value: undefined, done: true }
